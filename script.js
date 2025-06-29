cbb475f21eaa5e949e085f32ed47b6648dbc84f4
dashhL2 <dashh_r2@proton.me>
2025-06-29 11:37:34 -0600

    dashh2

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ const textos = [
 const velocidadEscritura = 60;
 const velocidadBorrado = 40;
 const titulo = "💕 Aloooo, Mi Vida 💕";
-const textoLargo = `, 
-`;
+const textoLargo = `Buenos diaaaas, mi niña linda, mi vida entera, mi mamu pro, el amor de todas mis vidaaas. 
+Como descanso la niña maas maaas preciosisisma, perfecta y pro de todo el universo? Espero que muy pero que muy bien
+y no torcida, amorcito. Solo queria recordarte que eres lo mas importante para mi, mi vida, estos dos ultimos meses me la he pasado
+muyyy pero que muuuyy bien contigo mi niña, han sido los mejores dos meses de mi vida porque estoy con mi niña preciosisima, te amo muchisisismo mi niña
+eres mi todo, quiero tooodo contigo amorcito, se que lo repito mucho pero ya no me puedo imaginar una vida sin ti, mi niña, me encantas demasiado, amo todo de ti
+tu forma de ser, de mirar, de todo mi niña, toodo de ti me encanta, espero que te hayas despertado feliz
+y lista para tener un dia muy muyy pro asi como tu mi niña, te amooooo 💞`;
 
 let i = 0; 
 let j = 0; 
@@ -53,6 +58,7 @@ function respuesta(opcion) {
   const estela = document.querySelector(".estela-rectangulo");
   const ss = document.querySelector(".nao1");
   const dashh = document.querySelector(".dashh")
+ 
 
   if (opcion === "si") {
     card.classList.add("mover-derecha");
@@ -86,8 +92,10 @@ function escribirTitulo(i = 0) {
   } else {
 
     const textoDiv = document.getElementById('dashh-text');
+     const dashhb = document.getElementById("show-video-button");
     textoDiv.innerHTML = textoLargo;
     textoDiv.style.opacity = 1;
+    dashhb.style.opacity = 1;
   }
 }
 
